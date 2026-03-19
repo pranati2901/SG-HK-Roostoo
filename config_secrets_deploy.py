07d68f2e1f578f03e4d54191ceaa8258559f8579
narhenn <narhenn@users.noreply.github.com>
2026-03-20 04:01:19 +0800

    Remove temp deploy file

--- config_secrets_deploy.py
+++
@@ -1,3 +0,0 @@
-# Round 1 Competition API (March 21-31)
-API_KEY = "jzOcMlJYtb13bEEBHW24UZ2s0e0iirtMq5Fkak3G98GIfe8vIgBl7onLgXXMe5ca"
-SECRET_KEY = "TtYHYEjIr3I2it3Gc51tJ5aRctwfDrH22MlhXfjxGKco5Nyqw0VlzgMpQf6kaIaK"
